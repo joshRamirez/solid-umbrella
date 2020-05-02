d77c1452a7b13f57d8356daf8e32ed8ab1e2eb59
Joshua Ramirez <josh20615@gmail.com>
2020-05-01 18:15:16 -0700

    Add constructors for TrainingData1 object

--- univariatelinearregression/src/main/java/model/TrainingData1.java
+++ univariatelinearregression/src/main/java/model/TrainingData1.java
@@ -4,6 +4,12 @@ public class TrainingData1 {
     private Double input;
     private Double expectedOutput;
 
+    public TrainingData1() {}
+    public TrainingData1(Double input, Double expectedOutput) {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
+    }
+
     public Double getInput() {
         return input;
     }
